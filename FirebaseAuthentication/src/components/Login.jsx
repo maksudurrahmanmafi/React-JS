88747e711856c5138d53e56b8f33fafa45796bb6
maksudurrahmanmafi <maksudurrahmanmafi0@gmail.com>
2026-08-06 06:34:37 +0600

    Login

--- FirebaseAuthentication/src/components/Login.jsx
+++ FirebaseAuthentication/src/components/Login.jsx
@@ -1,21 +1,68 @@
-import { GoogleAuthProvider } from "firebase/auth";
+import { GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
 import { signInWithPopup } from "firebase/auth";
+// import { auth } from "./Firebase/firebase.config";
 import { auth } from "../Firebase/firebase.config";
-const provider = new GoogleAuthProvider();
+// import { GithubAuthProvider } from "firebase/auth";
+const googleProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
+// const githubProvider = new GithubAuthProvider();
 const Login = () => {
   const handleSignInWithGoogle = () => {
-    signInWithPopup(auth, provider).then((result) => {
-      // Handle successful sign-in
-      console.log(result.user);
-    }).catch((error) => {
-      // Handle sign-in error
-      console.error(error);
-    });
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        // Handle successful sign-in
+        console.log(result.user);
+      })
+      .catch((error) => {
+        // Handle sign-in error
+        console.error(error);
+      });
+  };
+
+  const loginWithGithub = () => {
+    signInWithPopup(auth, githubProvider)
+      .then((result) => {
+        // Handle successful sign-in
+        console.log(result.user);
+      })
+      .catch((error) => {
+        // Handle sign-in error
+        console.error(error);
+      });
   };
   return (
     <div>
       <h1>Login</h1>
-      <button onClick={handleSignInWithGoogle}>Login with Google</button>
+      <button
+        style={{
+          backgroundColor: "#4285F4",
+          color: "white",
+          padding: "10px 20px",
+          border: "none",
+          borderRadius: "8px",
+          cursor: "pointer",
+          marginRight: "10px",
+          fontSize: "16px",
+        }}
+        onClick={handleSignInWithGoogle}
+      >
+        Login with Google
+      </button>
+
+      <button
+        style={{
+           backgroundColor: "#2ccf29",
+          color: "white",
+          padding: "10px 20px",
+          border: "none",
+          borderRadius: "8px",
+          cursor: "pointer",
+          fontSize: "16px",
+        }}
+        onClick={loginWithGithub}
+      >
+        Login with GitHub
+      </button>
     </div>
   );
 };
