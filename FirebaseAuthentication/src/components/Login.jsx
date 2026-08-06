e29c703db2fc69bd0123cf32b7bc5b6461b69594
maksudurrahmanmafi <maksudurrahmanmafi0@gmail.com>
2026-08-06 09:08:56 +0600

    log in with email and password

--- FirebaseAuthentication/src/components/Login.jsx
+++ FirebaseAuthentication/src/components/Login.jsx
@@ -1,4 +1,4 @@
-import { GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
+import { GoogleAuthProvider, GithubAuthProvider , createUserWithEmailAndPassword} from "firebase/auth";
 import { signInWithPopup } from "firebase/auth";
 // import { auth } from "./Firebase/firebase.config";
 import { auth } from "../Firebase/firebase.config";
@@ -30,40 +30,86 @@ const Login = () => {
         console.error(error);
       });
   };
-  return (
-    <div>
-      <h1>Login</h1>
-      <button
-        style={{
-          backgroundColor: "#4285F4",
-          color: "white",
-          padding: "10px 20px",
-          border: "none",
-          borderRadius: "8px",
-          cursor: "pointer",
-          marginRight: "10px",
-          fontSize: "16px",
-        }}
-        onClick={handleSignInWithGoogle}
-      >
-        Login with Google
-      </button>
 
-      <button
-        style={{
-           backgroundColor: "#2ccf29",
-          color: "white",
-          padding: "10px 20px",
-          border: "none",
-          borderRadius: "8px",
-          cursor: "pointer",
-          fontSize: "16px",
-        }}
-        onClick={loginWithGithub}
-      >
-        Login with GitHub
-      </button>
-    </div>
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    createUserWithEmailAndPassword(auth,email,password).then( res =>{
+      console.log(res.user)
+    }).catch(err =>{
+      console.log(err.message)
+    })
+  };
+  return (
+    <>
+      <div>
+        <h1>Login</h1>
+        <button
+          style={{
+            backgroundColor: "#4285F4",
+            color: "white",
+            padding: "10px 20px",
+            border: "none",
+            borderRadius: "8px",
+            cursor: "pointer",
+            marginRight: "10px",
+            fontSize: "16px",
+          }}
+          onClick={handleSignInWithGoogle}
+        >
+          Login with Google
+        </button>
+
+        <button
+          style={{
+            backgroundColor: "#2ccf29",
+            color: "white",
+            padding: "10px 20px",
+            border: "none",
+            borderRadius: "8px",
+            cursor: "pointer",
+            fontSize: "16px",
+          }}
+          onClick={loginWithGithub}
+        >
+          Login with GitHub
+        </button>
+      </div>
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Login now!</h1>
+          </div>
+          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <div className="card-body">
+              <form onSubmit={handleSubmit}>
+                <fieldset className="fieldset">
+                  <label className="label">Email</label>
+                  <input
+                    type="email"
+                    className="input"
+                    name="email"
+                    placeholder="Email"
+                  />
+                  <label className="label">Password</label>
+                  <input
+                    type="password"
+                    className="input"
+                    name="password"
+                    placeholder="Password"
+                  />
+                  <div>
+                    <a className="link link-hover">Forgot password?</a>
+                  </div>
+                  <button className="btn btn-neutral mt-4">Register</button>
+                </fieldset>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
--- FirebaseAuthentication/src/main.jsx
+++ FirebaseAuthentication/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "./app.css";
 import Login from "./components/Login.jsx";
 import Home from "./components/Home.jsx";
 import {
