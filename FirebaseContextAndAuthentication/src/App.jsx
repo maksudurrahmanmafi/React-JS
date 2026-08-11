2da66d2dab9914fca462310761fe2f0fae1a77cd
maksudurrahmanmafi <maksudurrahmanmafi0@gmail.com>
2026-08-11 08:14:05 +0600

    Home

--- FirebaseContextAndAuthentication/src/App.jsx
+++ FirebaseContextAndAuthentication/src/App.jsx
@@ -1,16 +1,32 @@
+import "./App.css";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "./Firebase/firebase.config";
 
-import './App.css'
+const googleProvider = new GoogleAuthProvider();
 
+const singInWithGoogle = () => {
+  signInWithPopup(auth, googleProvider)
+    .then((res) => {
+      console.log(res.user);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 function App() {
-
-
   return (
     <>
-     <h1 className="text-3xl font-bold underline text-center">
-        Hello Firebase!
-      </h1>
+      <div className="flex flex-col items-center gap-4">
+        <h1 className="text-3xl font-bold underline text-center hover:text-blue-500 hover:cursor-pointer">
+          Hello Firebase!
+        </h1>
+
+        <button onClick={singInWithGoogle} className="btn btn-primary">
+          Sign In with Google
+        </button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- FirebaseContextAndAuthentication/src/main.jsx
+++ FirebaseContextAndAuthentication/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
 import './index.css'
 
 import { createBrowserRouter } from "react-router";
@@ -8,7 +9,7 @@ import { RouterProvider } from "react-router/dom";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component: App,               
   },
 ]);
 createRoot(document.getElementById('root')).render(
