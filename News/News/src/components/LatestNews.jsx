f8768a492137693b0e05b234a5c1f1b82ac5ccb0
maksudurrahmanmafi <maksudurrahmanmafi0@gmail.com>
2026-08-18 19:59:05 +0600

    Marqueue text

--- News/News/src/components/LatestNews.jsx
+++ News/News/src/components/LatestNews.jsx
@@ -1,14 +1,24 @@
-
+// import Marquee from "react-fast-marquee";
 
 const LatestNews = () => {
-    return (
-        <div className="flex items-center gap-3 bg-base-200">
-            <p className="text-base-100 bg-secondary px-4 py-3" >Latest</p>
-            <p className="font-bold">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod id dolorem rem perferendis recusandae ex voluptate sunt hic quis consequatur.
-            </p>
-        </div>
-    );
+  return (
+    <div className="flex items-center gap-3 bg-base-200">
+      <p className="text-base-100 bg-secondary px-4 py-3">Latest</p>
+
+      <p className="font-bold">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod id dolorem
+        rem perferendis recusandae ex voluptate sunt hic quis consequatur.
+      </p>
+      <p className="font-bold">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod id dolorem
+        rem perferendis recusandae ex voluptate sunt hic quis consequatur.
+      </p>
+      <p className="font-bold">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod id dolorem
+        rem perferendis recusandae ex voluptate sunt hic quis consequatur.
+      </p>
+    </div>
+  );
 };
 
 export default LatestNews;
